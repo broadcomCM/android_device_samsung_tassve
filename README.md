db6e5eed1706e7251722c01830e65dc294b8f32a
bieltv3 <bieltv.3@gmail.com>
2013-03-01 11:22:30 +0100

    add cyanogen_tassve + instructions to build

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Device tree for Samsung Galaxy Mini i S5570i - CM7
 Maintainer: Bieltv.3
 
 Special Thanks: Phiexz
+
+Note: to build you must paste cyanogen_tassve.mk to vendor/cyanogen/products
---
+++ cyanogen_tassve.mk
@@ -0,0 +1,36 @@
+# Inherit AOSP device configuration for blade.
+$(call inherit-product, device/samsung/tassve/device_tassve.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Include FM-Radio stuff
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_tassve
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := tassve
+PRODUCT_MODEL := GT-S5570i
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-S5570i BUILD_ID=GRI40 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=samsung/GT-S5570i/GT-S5570i:2.3.6/GINGERBREAD/XWKTN:user/release-keys PRIVATE_BUILD_DESC="GT-S5570i-user 2.3.6 GINGERBREAD XWKTN release-keys"
+
+# Add LDPI assets, in addition to MDPI
+PRODUCT_LOCALES += ldpi mdpi
+
+# Extra overlay for LDPI
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/mdpi
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+     vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := GalaxyMinii
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
